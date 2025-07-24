fc92fc923f02e78863aaf36d8e59707458c00f57
Rakshitarathod <rcrathod1562006@gmail.com>
2025-07-24 20:31:26 +0530

    Update Main.java

--- src/main/java/com/codtech/recommendation/Main.java
+++ src/main/java/com/codtech/recommendation/Main.java
@@ -7,9 +7,12 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter User ID: ");
+        int userId = scanner.nextInt();
         RecommenderEngine engine = new RecommenderEngine();
         try {
-            engine.generateRecommendations("src/main/resources/data.csv", 1, 3);  // userID = 1, top 3 recommendations
+            engine.generateRecommendations("src/main/resources/data.csv", userID, 3);  // userID = 1, top 3 recommendations
         } catch (Exception _) {
 
         }
